b37601fba65c026e1979a90f33036a4f42bbbf00
CoolerProYT <veronlam1818@gmail.com>
2024-04-24 12:51:15 +0800

    DirectPayment

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # RazerMS-PHP
+
+To install the package, run the following command:
+```bash
+composer require coolerproyt/razerms-php
+```
+
+In your PHP file, include the following code at the top of your code:
+```php
+namespace CoolerproYT\RazermsPHP;
+require_once 'vendor/autoload.php';
+```
---
+++ src/DirectPayment.php
@@ -0,0 +1,54 @@
+<?php
+namespace CoolerProYT\RazermsPHP;
+
+use http\Exception\InvalidArgumentException;
+
+class DirectPayment
+{
+    private string $secret_key;
+    private string $verify_key;
+    public string $url;
+
+    public function __construct($secret_key,$verify_key,$sandbox = false)
+    {
+        $this->secret_key = $secret_key;
+        $this->verify_key = $verify_key;
+        $this->url = $sandbox ? 'https://sandbox.merchant.razer.com' : 'https://pay.merchant.razer.com';
+    }
+
+    public function createPayment(array $data, $autoRedirect = false){
+        $mandatory = ['MerchantID','ReferenceNo','TxnType','TxnChannel','TxnCurrency','TxnAmount','Signature'];
+
+        foreach($mandatory as $key){
+            if(!array_key_exists($key,$data)){
+                throw new InvalidArgumentException("Mandatory key $key is missing");
+            }
+        }
+
+        $curl = curl_init($this->url . '/RMS/API/Direct/latest/index.php');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+        $response = json_decode(curl_exec($curl));
+        curl_close($curl);
+
+        if(!$autoRedirect) return json_encode($response);
+        if(isset($response->status)) return json_encode($response);
+
+        $this->redirectToPaymentPage($response->TxnData);
+    }
+
+    public function redirectToPaymentPage(\stdClass $data){
+        $form = '<form id="paymentForm" action="' . $data->RequestURL . '" method="' . $data->RequestMethod .'">';
+        foreach ($data->RequestData as $key => $value) {
+            $form .= '<input type="hidden" name="' . $key . '" value="' . $value . '">';
+        }
+        $form .= '</form>';
+
+        echo $form;
+        echo '<script>document.getElementById("paymentForm").submit();</script>';
+    }
+
+}
+?>
